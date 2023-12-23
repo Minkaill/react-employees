9aa502aade3279db7413bc882713aa0280a63398
Minkail <mkx.dake@gmail.com>
2023-12-23 13:32:15 +0300

    Update auth.tsx

--- client/src/features/auth/auth.tsx
+++ client/src/features/auth/auth.tsx
@@ -1,9 +1,10 @@
 import { useCurrentQuery } from "../../app/services/auth/auth";
+import { Spin } from 'antd';
 
 export const Auth = ({ children }: { children: JSX.Element }) => {
   const { isLoading } = useCurrentQuery();
 
-  if (isLoading) return <span>Загрузка...</span>;
+  if (isLoading) return <span style={{width: "100vw", height: "80vh", margin: "auto"}}><Spin /></span>;
 
   return children;
 };
